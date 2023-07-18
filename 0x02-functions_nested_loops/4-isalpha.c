d3d53764821c5e3613724d94284c6a581e5f3ced
asayeayal <asaye.ayal@gmail.com>
2023-07-18 05:45:09 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,7 +2,7 @@
 
 /**
  *
- * _isalph - Checks for the alphabetic character
+ * _isalpha - Checks for the alphabetic character
  * @c: The character to be checked
  * Return: 1 for alphabetic character or 0 for any else
  */
