0aa9c37c7627deddc638a14b52adde5f939e18f2
asayeayal <asaye.ayal@gmail.com>
2023-07-18 12:17:52 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -4,10 +4,11 @@
 
 /**
  * print_to_98 - Prints all natural numbers from n to 98
- * * Return: Always 0.
+ * Return: Always 0.
  */
 
 void print_to_98(int n)
+
 {
 
 	if (n <= 98)
@@ -42,4 +43,5 @@ void print_to_98(int n)
 	}
 	}
 	}
+
 }
