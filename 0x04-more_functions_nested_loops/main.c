0f5c763425cc8499045a2bc0a0c36728329c072f
asayeayal <asaye.ayal@gmail.com>
2023-07-21 03:28:16 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/main.c
@@ -0,0 +1,12 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+int _putchar(char c);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+
+#endif
