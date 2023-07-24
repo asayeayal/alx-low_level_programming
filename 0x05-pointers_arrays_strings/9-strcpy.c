bbd31f957bc0cc9aa002b8dfd3a78afaf3b6458f
asayeayal <asaye.ayal@gmail.com>
2023-07-24 06:13:24 -0700

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strcpy - copy string
+ * @dest: destination value
+ * @scr: source value
+ * Return: pointer to dest
+ */
+
+char *_strcpy(char *dest, char *scr)
+{
+	int x;
+	for (x = 0; scr[x] != '\0'; x++)
+	{
+	dest[x] = scr[x];
+	}
+	dest[x++] = '\0';
+	return (dest);
+}
