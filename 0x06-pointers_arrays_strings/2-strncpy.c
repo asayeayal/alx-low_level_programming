16686390640a4a8acfa8e3f5ad1c09dc8904e91e
asayeayal <asaye.ayal@gmail.com>
2023-07-27 02:36:52 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -7,10 +7,9 @@
  * @src: the source string
  * @n: maximum number of bytes coppied from src
  * Return: pointer to the resulting string dest
- *
  */
-
 char *_strncpy(char *dest, char *src, int n)
+
 {
 	int index = 0, src_len = 0;
 
