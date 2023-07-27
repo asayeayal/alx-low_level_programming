76b64b3395065beeb48458f8a3941e9342b3fa2f
asayeayal <asaye.ayal@gmail.com>
2023-07-27 01:43:23 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,8 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <stdio.h>
 
-int _putchar(char c);
 char *_strcat(char *dest, char *scr);
 char *_strncat(char *dest, char *scr, int n);
 char *_strncpy(char *dest, char *scr, int n);
@@ -15,4 +15,5 @@ char *rot13(char *);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
+
 #endif
