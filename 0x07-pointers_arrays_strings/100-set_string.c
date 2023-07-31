ff9964a9e357ed739e22db300564c408a77fbab9
asayeayal <asaye.ayal@gmail.com>
2023-07-31 04:29:02 -0700

    100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * set_string - begins executing entry point
+ * @s: inputs
+ * @to: inputs
+ * Return: Always 0 (Success)
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
