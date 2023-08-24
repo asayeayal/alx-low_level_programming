27a7dc508a4fac41af3a6829cbdc7a3b790076f8
asayeayal <asaye.ayal@gmail.com>
2023-08-24 04:31:07 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * bmain - ...
+ * Return: ...
+ */
+void __attribute__((constructor)) bmain()
+{
+	printf("You're beat! and yet, you must allow");
+	printf(",\nI bore my house upon my back!\n");
+}
