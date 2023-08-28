be4ba1f7bb6c2c91b1eb019aecc454888ae1640d
asayeayal <asaye.ayal@gmail.com>
2023-08-28 03:50:55 -0700

    2-add_nodeint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,17 +1,18 @@
 #include "lists.h"
+
 /**
  * print_listint - ...
  * @h: ...
  *
  * Return: ...
  */
-size_t print_listint(const listint_t *h);
+size_t print_listint(const listint_t *h)
 {
 	size_t num = 0;
 
 	while (h)
 	{
-		printf("d\n", h->n);
+		printf("%d\n", h->n);
 		num++;
 		h = h->next;
 	}
