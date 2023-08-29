0f58998ea68f82098cc0c21d819784b485d7f168
asayeayal <asaye.ayal@gmail.com>
2023-08-29 03:13:46 -0700

    1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,19 +1,19 @@
 #include "lists.h"
-#include <stdio.h>
+
 /**
- * listin_len - ...
+ * listint_len - ...
  * @h: ...
  *
  * Return: ...
  */
 size_t listint_len(const listint_t *h)
 {
-	size_t nodes = 0;
+	size_t num = 0;
 
-	while (h)
+	while (h != NULL)
 	{
-		nodes++;
+		num++;
 		h = h->next;
 	}
-	return (nodes);
+	return (num);
 }
